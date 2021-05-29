149141dbb572fef605be4bfcad052ce6faafc9ab
MartinSlooter <martin.slooter@outlook.com>
2021-05-29 10:16:16 +0200

    Nav finished

---
+++ 51-floating-nav/script.js
@@ -0,0 +1,14 @@
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("header");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    console.log(window.pageYOffset, sticky);
+
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky")
+  } else {
+    header.classList.remove("sticky");
+  }
+}
